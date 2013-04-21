be70f066be943717240d356da9be11fdb3946173
zava <zava.kid@gmail.com>
2013-04-21 10:18:23 +0800

    add LICENSE

--- src/main/java/com/zavakid/paxos/example/ChooseMaster.java
+++ src/main/java/com/zavakid/paxos/example/ChooseMaster.java
@@ -37,11 +37,11 @@ public class ChooseMaster {
         Proposer proposer2 = new DefaultProposer(acceptors, 2, acceptors.size(), 5);
         Proposer proposer3 = new DefaultProposer(acceptors, 3, acceptors.size(), 5);
 
-        Object value2 = proposer2.propose("master", "node_2");
-        System.out.println(value2);
-
         Object value = proposer1.propose("master", "node_1");
         System.out.println(value);
+
+        Object value2 = proposer2.propose("master", "node_2");
+        System.out.println(value2);
 
         Object value3 = proposer3.propose("master", "node_3");
         System.out.println(value3);
